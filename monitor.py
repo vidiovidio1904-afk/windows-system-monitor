403017df529470ad9a57063089531882d2f7c234
Alexxx19 <vidiovidio1904@gmail.com>
2026-06-18 21:12:24 +0300

    Add basic GUI for Windows System Monitor

--- monitor.py
+++ monitor.py
@@ -1,1 +1,16 @@
+import tkinter as tk
 
+root = tk.Tk()
+
+root.title("Windows System Monitor")
+root.geometry("600x400")
+
+label = tk.Label(
+    root,
+    text="Windows System Monitor",
+    font=("Arial", 20)
+)
+
+label.pack(pady=50)
+
+root.mainloop()
